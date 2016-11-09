813cb351ba8f2602dc51e89c22b9ccdfc9421f2e
[Huangtianhui] <[huihuijiang0619@gmail.com]>
2016-11-08 20:45:36 -0500

    insert postion

---
+++ src/Search_Insert_Position_35/last_postion.java
@@ -0,0 +1,43 @@
+package Search_Insert_Position_35;
+
+/**
+ * Created by tianhuihuang on 2016-11-08.
+ */
+// version 2: find the last position < target, return +1， 要特判一下target小于所有数组里面的元素
+
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int start = 0;
+        int end = A.length - 1;
+        int mid;
+
+        if (target < A[0]) {
+            return 0;
+        }
+        // find the last number less than target
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                return mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (A[end] == target) {
+            return end;
+        }
+        if (A[end] < target) {
+            return end + 1;
+        }
+        if (A[start] == target) {
+            return start;
+        }
+        return start + 1;
+    }
+}
